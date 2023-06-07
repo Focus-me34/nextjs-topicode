0ec4fe4ea72b899989ec4d14a52149cb20ea0433
aminmallol <aminm-34@hotmail.fr>
2023-06-07 10:12:07 +0100

    The data is properly fetched to get users using RTK query. Need to leanr more about it but i don't think I'll be using it as it is good for simple projects. But probably not for more complex ones

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
-import topicReducer from "./features/topicSlice";
 import counterReducer from "./features/counterSlice";
+import { userApi } from "./services/userApi";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+
+import topicReducer from "./features/topicSlice";
 
 export const store = configureStore({
   reducer: {
     counterReducer,
     topicReducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({}).concat([userApi.middleware]),
 });
+
+setupListeners(store.dispatch);
+
 
 // export type RootState = ReturnType<typeof store.getState>;
 // export type AppDispatch = typeof store.dispatch;
