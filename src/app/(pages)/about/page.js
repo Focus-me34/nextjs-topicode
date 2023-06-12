0355654441e8e9b5848470b0fdcab185de7e9e03
aminmallol <aminm-34@hotmail.fr>
2023-06-12 18:28:38 +0100

    Manage to add my first meta tags (hard coded values) to the about page. Checked in the source code and it weems to work perfectly. I'm not using the Head component but a metadata object. It seems that nextjs knows that this object is suppoed to be considered as meta tags

--- src/app/(pages)/about/page.js
+++ src/app/(pages)/about/page.js
@@ -1,20 +1,18 @@
 import Head from "next/head";
 
+export const metadata = {
+  title: "About page",
+  description: "About page",
+  keywords: "about, nextjs"
+}
+
 const About = () => {
   return (
-  <>
-    <Head>
-      <title>About page</title>
-      <meta name="description" content="About page" />
-      <meta name="keywords" content="about, nextjs" />
-    </Head>
-
     <div>
       <h2>About page</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus magnam doloremque ratione ullam iure facilis, omnis quos molestias placeat, ducimus temporibus accusantium maxime non architecto repellendus nisi perspiciatis soluta tempora!</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus magnam doloremque ratione ullam iure facilis, omnis quos molestias placeat, ducimus temporibus accusantium maxime non architecto repellendus nisi perspiciatis soluta tempora!</p>
     </div>
-  </>
   );
 }
 
